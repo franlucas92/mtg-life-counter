c854b5468a8b1b10cb724d3305c5b5c03ff2cbf5
Fran Lucas <franlucas@pop-os.localdomain>
2024-10-23 14:43:05 +0200

    Inicial

---
+++ server.js
@@ -0,0 +1,37 @@
+// server.js
+const { createServer } = require('http');
+const next = require('next');
+const socketIo = require('socket.io');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = createServer((req, res) => {
+    handle(req, res);
+  });
+
+  const io = socketIo(server);
+
+  io.on('connection', (socket) => {
+    console.log('Nuevo cliente conectado');
+
+    socket.on('joinGame', (gameId) => {
+      socket.join(gameId);
+    });
+
+    socket.on('updateLife', ({ gameId, lifeData }) => {
+      io.to(gameId).emit('updateLife', lifeData);
+    });
+
+    socket.on('disconnect', () => {
+      console.log('Cliente desconectado');
+    });
+  });
+
+  server.listen(3000, (err) => {
+    if (err) throw err;
+    console.log('> Servidor listo en http://localhost:3000');
+  });
+});
